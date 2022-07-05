13c92e90d5ea9f55cc42f4f8ef441cba7fa6cc88
Alexandre Journet <alexandre.journet@gmail.com>
2022-07-05 23:07:09 +0200

    Change build
    Signed-off-by: Alexandre Journet <alexandre.journet@gmail.com>

--- update-version.js
+++ update-version.js
@@ -1,24 +1,22 @@
 let exec = require('child_process').exec;
+let package = require('./package.json');
 
+let old_version = package.version;
 let version = "";
 
-let cmd = exec("semver 0.0.0-alpha.0 -i prerelease", function (err, stdout, stderr) {
+let cmd = exec("semver " + old_version + " -i prerelease", function (err, stdout, stderr) {
   if (err) {
-    // handle error
+    console.log('err', err);
   }
 
   version = stdout;
-  console.log('version ', version);
-  version.replace("\\n", "");
 
   let cmd2 = exec("npm version " + version, function (err, stdout, stderr) {
     if (err) {
       console.log('err', err);
     }
 
-    console.log('out', stdout);
-    console.log('err', stderr);
-    console.log('version update to', version);
+    console.log('version update from ' + old_version + ' to ' + version);
 
   });
 
