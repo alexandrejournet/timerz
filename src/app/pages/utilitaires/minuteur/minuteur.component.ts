0f46596baf339ee94b73ebb40aaae2dff5bda6d0
Alexandre Journet <alexandre.journet@gmail.com>
2022-07-06 13:34:30 +0200

    Début minuteur
    Signed-off-by: Alexandre Journet <alexandre.journet@gmail.com>

--- src/app/pages/utilitaires/minuteur/minuteur.component.ts
+++ src/app/pages/utilitaires/minuteur/minuteur.component.ts
@@ -1,12 +1,21 @@
+import {CommonModule} from '@angular/common';
 import {Component, OnInit} from '@angular/core';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {faPlayCircle} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   standalone: true,
   selector: 'app-minuteur',
   templateUrl: './minuteur.component.html',
-  styleUrls: ['./minuteur.component.scss']
+  styleUrls: ['./minuteur.component.scss'],
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+  ]
 })
 export class MinuteurComponent implements OnInit {
+
+  public readonly playBtn = faPlayCircle;
 
   constructor() {
   }
